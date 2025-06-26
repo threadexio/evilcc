a8c3d4ba49c7003ac07be72b97cf2aff836756ae
threadexio <pzarganitis@gmail.com>
2025-06-26 17:12:04 +0300

    fix warnings in the debug build

--- src/arch/x86_64/syscalls.h
+++ src/arch/x86_64/syscalls.h
@@ -84,32 +84,37 @@
     return result;                                                                                     \
   }
 
-always_inline static syscall0(__NR_getegid, gid_t, _getegid);
-always_inline static syscall0(__NR_geteuid, uid_t, _geteuid);
-always_inline static syscall0(__NR_getgid, gid_t, _getgid);
-always_inline static syscall0(__NR_getuid, uid_t, _getuid);
-always_inline static syscall1(__NR_personality, int, _personality, unsigned long, persona);
-always_inline static syscall2(__NR_chmod, int, _chmod, const char*, pathname, mode_t, mode);
-always_inline static syscall2(__NR_kill, int, _kill, pid_t, pid, int, sig);
-always_inline static syscall2(__NR_stat, int, _stat, const char*, pathname, struct stat*, statbuf);
-always_inline static syscall3(__NR_execve, int, _execve, const char*, pathname, const char**, argv, const char**, envp);
-always_inline static syscall3(__NR_setresgid, int, _setresgid, gid_t, rgid, gid_t, egid, gid_t, sgid);
-always_inline static syscall3(__NR_setresuid, int, _setresuid, uid_t, ruid, uid_t, euid, uid_t, suid);
-always_inline static syscall3(__NR_write, ssize_t, _write, int, fd, const void*, buf, size_t, len);
-always_inline static syscall5(__NR_prctl, int, _prctl, int, option, unsigned long, arg2, unsigned long, arg3, unsigned long, arg4, unsigned long, arg5);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-function"
 
-#define chmod(pathname, mode) _chmod(pathname, mode)
-#define execve(pathname, argv, envp) _execve(pathname, argv, envp)
-#define getegid() _getegid()
-#define geteuid() _geteuid()
-#define getgid() _getgid()
-#define getuid() _getuid()
-#define kill(pid, sig) _kill(pid, sig)
-#define personality(persona) _personality(persona)
-#define prctl(option, arg2, arg3, arg4, arg5) _prctl(option, arg2, arg3, arg4, arg5)
-#define setresgid(rgid, egid, sgid) _setresgid(rgid, egid, sgid)
-#define setresuid(ruid, euid, suid) _setresuid(ruid, euid, suid)
-#define stat(pathname, statbuf) _stat(pathname, statbuf)
-#define write(fd, buf, len) _write(fd, buf, len)
+always_inline static syscall0(__NR_getegid, gid_t, __evilcc_getegid);
+always_inline static syscall0(__NR_geteuid, uid_t, __evilcc_geteuid);
+always_inline static syscall0(__NR_getgid, gid_t, __evilcc_getgid);
+always_inline static syscall0(__NR_getuid, uid_t, __evilcc_getuid);
+always_inline static syscall1(__NR_personality, int, __evilcc_personality, unsigned long, persona);
+always_inline static syscall2(__NR_chmod, int, __evilcc_chmod, const char*, pathname, mode_t, mode);
+always_inline static syscall2(__NR_kill, int, __evilcc_kill, pid_t, pid, int, sig);
+always_inline static syscall2(__NR_stat, int, __evilcc_stat, const char*, pathname, struct stat*, statbuf);
+always_inline static syscall3(__NR_execve, int, __evilcc_execve, const char*, pathname, const char**, argv, const char**, envp);
+always_inline static syscall3(__NR_setresgid, int, __evilcc_setresgid, gid_t, rgid, gid_t, egid, gid_t, sgid);
+always_inline static syscall3(__NR_setresuid, int, __evilcc_setresuid, uid_t, ruid, uid_t, euid, uid_t, suid);
+always_inline static syscall3(__NR_write, ssize_t, __evilcc_write, int, fd, const void*, buf, size_t, len);
+always_inline static syscall5(__NR_prctl, int, __evilcc_prctl, int, option, unsigned long, arg2, unsigned long, arg3, unsigned long, arg4, unsigned long, arg5);
+
+#pragma GCC diagnostic pop
+
+#define chmod(pathname, mode) __evilcc_chmod(pathname, mode)
+#define execve(pathname, argv, envp) __evilcc_execve(pathname, argv, envp)
+#define getegid() __evilcc_getegid()
+#define geteuid() __evilcc_geteuid()
+#define getgid() __evilcc_getgid()
+#define getuid() __evilcc_getuid()
+#define kill(pid, sig) __evilcc_kill(pid, sig)
+#define personality(persona) __evilcc_personality(persona)
+#define prctl(option, arg2, arg3, arg4, arg5) __evilcc_prctl(option, arg2, arg3, arg4, arg5)
+#define setresgid(rgid, egid, sgid) __evilcc_setresgid(rgid, egid, sgid)
+#define setresuid(ruid, euid, suid) __evilcc_setresuid(ruid, euid, suid)
+#define stat(pathname, statbuf) __evilcc_stat(pathname, statbuf)
+#define write(fd, buf, len) __evilcc_write(fd, buf, len)
 
 #endif
